cdcd34b4373123949d1bb71843f7c561b3bed524
khanaman007 <khan.aman007@gmail.com>
2025-02-20 23:47:27 -0500

    with github action file with some minor change

--- src/main/java/com/gcp/alloy/demo/Alloy/db/entity/Customer.java
+++ src/main/java/com/gcp/alloy/demo/Alloy/db/entity/Customer.java
@@ -7,7 +7,25 @@ import jakarta.persistence.Id;
 public class Customer {
     @Id
     private Long id;
-    private String name;
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String name;
     private String email;
 
     // Getters and Setters
